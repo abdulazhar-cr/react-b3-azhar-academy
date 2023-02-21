49c80e6fc0c32ab17f73bc8d00b9959ab5479589
abdulazhar-cr <116333819+abdulazhar-cr@users.noreply.github.com>
2023-02-21 08:41:38 +0530

    feat added to main

---
+++ day04/closure-imediatly-invoked.js
@@ -0,0 +1,4 @@
+const text = "learn and grow";
+void (function displayName(name1) {
+  console.log("Azhar Academy", name1);
+})(text);
---
+++ day04/regular-vs-arrow-function.js
@@ -0,0 +1,5 @@
+// Regular Function gives access to the calling function (this)
+// Vs
+// Arrow function gives access to their defining environment(this)
+
+// check the example of html in day 26
